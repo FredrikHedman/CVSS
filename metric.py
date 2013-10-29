8614ff03d7e4ca52371e1a6c2301d22043321af8
Fredrik B. Hedman <fredrik.hedman@noruna.se>
2013-10-29 21:32:36 +0100

    Added string conversion functions.

--- metric.py
+++ metric.py
@@ -4,19 +4,18 @@
 
 >>> from metric_value import MetricValue
 >>> values = [ MetricValue('Multiple', 'M', 1.11, 'Exploiting the vulnerability...'), \
-                   MetricValue('Single', 'S', 2.12, 'The vulnerability requires...'), \
-                   MetricValue('None', 'N', 3.13, 'Authentication is not required...'), ]
+               MetricValue('Single', 'S', 2.12, 'The vulnerability requires...'), ]
 >>> m = Metric("Authentication", "Au", values, 1)
 >>> m.name
 'Authentication'
 >>> m.short_name
 'Au'
 >>> m.values
-[MetricValue('Multiple','M',1.11,'Exploiting the vulnerability...'), MetricValue('Single','S',2.12,'The vulnerability requires...'), MetricValue('None','N',3.13,'Authentication is not required...')]
+[MetricValue('Multiple','M',1.11,'Exploiting the vulnerability...'), MetricValue('Single','S',2.12,'The vulnerability requires...')]
 >>> m.value = 4
 Traceback (most recent call last):
 ...
-AssertionError: must be in range [0, 3[
+AssertionError: must be in range [0, 2[
 >>> m.value = 1
 >>> m.value
 MetricValue('Single','S',2.12,'The vulnerability requires...')
@@ -24,6 +23,10 @@ MetricValue('Single','S',2.12,'The vulnerability requires...')
 S
 >>> float(m.value)
 2.12
+>>> print(m)
+Au:S
+>>> repr(m)
+"Metric('Authentication','Au',[MetricValue('Multiple','M',1.11,'Exploiting the vulnerability...'), MetricValue('Single','S',2.12,'The vulnerability requires...')],'S')"
 """
 
 class Metric:
@@ -32,6 +35,16 @@ class Metric:
         self.__abbr = short_name
         self.__values = tuple(metric_values)
         self.value = value
+
+    def __str__(self):
+        return "{0}:{1}".format(self.short_name, self.value)
+
+    def __repr__(self):
+        return ("{0}('{1}','{2}',{3},'{4}')".format(self.__class__.__name__,
+                                              self.name,
+                                              self.short_name,
+                                              self.values,
+                                                    self.value))
 
     @property
     def name(self):
