2f470fdb11ecec9eb5f15e9eb57b3f4e70124dcc
Fredrik B. Hedman <fredrik.hedman@noruna.se>
2013-10-29 13:25:39 +0100

    A no-op straw man.

--- cvss_calci3.py
+++ cvss_calci3.py
@@ -6,7 +6,27 @@ Formulas are extracted from http://www.first.org/cvss/cvss-guide.html
 and tests are based on the examples shown in this document.
 """
 import argparse
+def cvss(parser):
+    args = parser.parse_args()
+    return args
 
-parser = argparse.ArgumentParser(description='Caculate CVSS v2 score.')
-args = parser.parse_args()
+def calculate_cvss_score(ibias):
+    return (0,0)
 
+def calculate_risk(tscore):
+    return None
+
+def calculate_vector(ibias):
+    return "foo/bar"
+
+def present_results(score, risk_factor, basescore_vector):
+    print(score, risk_factor, basescore_vector)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Caculate CVSS v2 score.')
+    impact_bias = cvss(parser)
+    score = calculate_cvss_score(impact_bias)
+    risk_factor = calculate_risk(score[1])
+    basescore_vector = calculate_vector(impact_bias)
+
+    present_results(score, risk_factor, basescore_vector)
