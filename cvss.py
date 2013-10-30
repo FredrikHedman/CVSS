2f4d30f2f219755542ea1e2a245dc5f4eb12deb3
Fredrik B. Hedman <fredrik.hedman@noruna.se>
2013-10-30 13:05:07 +0100

    Refactored impact and adjusted_impact.

--- cvss.py
+++ cvss.py
@@ -18,13 +18,17 @@ class CommonVulnerabilityScore:
             val = 0.0
         return val
 
+    def impact_fcn(self, conf_impact, integ_impact, avail_impact):
+        result = 1 - (1-conf_impact)*(1-integ_impact)*(1-avail_impact)
+        result *= 10.41
+        return result
+
     @property
     def impact(self):
         ConfImpact = float(self.metrics['C'])
         IntegImpact = float(self.metrics['I'])
         AvailImpact = float(self.metrics['A'])
-        result = 10.41*(1-(1-ConfImpact)*(1-IntegImpact)*(1-AvailImpact))
-        return result
+        return self.impact_fcn(ConfImpact, IntegImpact, AvailImpact)
 
     @property
     def exploitability(self):
@@ -60,14 +64,10 @@ class CommonVulnerabilityScore:
 
     @property
     def adjusted_impact(self):
-        ConfImpact = float(self.metrics['C'])
-        ConfReq = float(self.metrics['CR'])
-        IntegImpact = float(self.metrics['I'])
-        IntegReq = float(self.metrics['IR'])
-        AvailImpact = float(self.metrics['A'])
-        AvailReq = float(self.metrics['AR'])
-        result = (1-(1-ConfImpact*ConfReq)*(1-IntegImpact*IntegReq)*(1-AvailImpact*AvailReq))
-        result *= 10.41
+        ConfImpact = float(self.metrics['C']) * float(self.metrics['CR'])
+        IntegImpact = float(self.metrics['I']) * float(self.metrics['IR'])
+        AvailImpact = float(self.metrics['A']) * float(self.metrics['AR'])
+        result = self.impact_fcn(ConfImpact, IntegImpact, AvailImpact)
         return min(10.0, result)
 
     @property
